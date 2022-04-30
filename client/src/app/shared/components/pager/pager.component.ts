86101b63943639aa50317bc90f2dbc66880dc7da
henrio randriasoa <rioxdev@gmail.com>
2022-04-30 12:09:33 +0200

    child component for pager

---
+++ client/src/app/shared/components/pager/pager.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pager',
+  templateUrl: './pager.component.html',
+  styleUrls: ['./pager.component.scss']
+})
+export class PagerComponent implements OnInit {
+
+  @Input() totalCount = 0;
+  @Input() pageSize = 0;
+  @Output() pageChanged = new EventEmitter<number>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  onPagerChanged(event : any)
+  {
+    this.pageChanged.emit(event.page);
+  }
+
+}
--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -85,11 +85,13 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.loadProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.loadProducts();
   }
 
@@ -99,14 +101,17 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged($event: any) {
-    this.shopParams.pageNumber = $event.page;
-    this.loadProducts();
+    if (this.shopParams.pageNumber != $event)
+    {
+      this.shopParams.pageNumber = $event;
+      this.loadProducts();
+    }
   }
 
   onSearchClicked() {
     this.shopParams.search = this.txtSearch.nativeElement.value;
     
-    console.log(this.shopParams.search);
+    this.shopParams.pageNumber = 1;
 
     this.loadProducts();
   }
