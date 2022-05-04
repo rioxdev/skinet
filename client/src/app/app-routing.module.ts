e1c2b4e6601970b8dd08ebdcdca636c1d208e4db
rioxdev <rioxdev@gmail.com>
2022-05-04 16:53:47 +0200

    routing - stage2

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,9 +7,9 @@ import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'shop', component: ShopComponent },
-  { path: 'shop/:id', component: ProductDetailsComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
+
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -21,7 +21,6 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule
   ],
   providers: [],
