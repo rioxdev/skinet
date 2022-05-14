64c9e5e8f681b2cfa95e6596b5aed07ac6f8c44b
rioxdev <rioxdev@gmail.com>
2022-05-14 22:19:21 +0200

    error handling - stage1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { ProductItemComponent } from './shop/product-item/product-item.component';
@@ -7,6 +8,7 @@ import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'test-error', component: TestErrorComponent},
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
 
   { path: '**', redirectTo: '', pathMatch: 'full' }
